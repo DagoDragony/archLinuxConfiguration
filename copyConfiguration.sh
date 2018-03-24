304f9cfa2699cc0b93011906fa42e0c88d292e81
dago <Dago.Dragony@gmail.com>
2018-03-24 14:27:35 +0200

    initial changes

---
+++ copyConfiguration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+####################################
+#
+# Copies desired configuration from current user
+#
+####################################
+
+cp -r ~/.fonts/. .fonts/
+cp -r ~/.config/i3/config .config/i3/
