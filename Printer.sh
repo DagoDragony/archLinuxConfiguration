5603cc8ae73f5566cd1467d735147ab326d76351
DagoDragony <Dago.Dragony@gmail.com>
2020-04-03 00:24:52 +0300

    printer setup

---
+++ Printer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+sudo pacman -S cups
+# for printing into pdf
+sudo pacman -S cups-pdf
+
+# cupsd
+# on startup run cupsd
+# lpinfo -v should show that printer exists
+# http://localhost:631/ admin console
+# add printer through console
+
+
