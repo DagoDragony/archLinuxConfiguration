2cbdd9f118079f581dec87756c8213a88022c959
DagoDragony <Dago.Dragony@gmail.com>
2021-01-30 00:41:25 +0200

    follow redirection of mirorlist and filter lithuania servers

--- 00-preinstall.sh
+++ 00-preinstall.sh
@@ -4,9 +4,10 @@ echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download          "
 echo "-------------------------------------------------"
 timedatectl set-ntp true
+pacman -Sy
 pacman -S --noconfirm pacman-contrib # for ranking mirrors
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -sL "https://archlinux.org/mirrorlist/?country=LT&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 read a
 echo -e "\nInstalling prereqs...\n$HR"
