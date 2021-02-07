ae9cb3df29fafcc21d929cb7eb3f0e0ed96eb162
DagoDragony <Dago.Dragony@gmail.com>
2021-02-08 01:05:48 +0200

    refactored to use functions for easier debugging

--- 00-preinstall.sh
+++ 00-preinstall.sh
@@ -1,71 +1,79 @@
 #!/usr/bin/env bash
 
 set -v
-pac
-echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download          "
-echo "-------------------------------------------------"
-timedatectl set-ntp true
-pacman -Sy
-pacman -S pacman-contrib --noconfirm --needed # for ranking mirrors
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -sL "https://archlinux.org/mirrorlist/?country=LT&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-echo -e "\nInstalling prereqs...\n$HR"
+function setupMirrors {
+  echo "Setting up mirrors"
+  timedatectl set-ntp true
+  pacman -Sy
+  pacman -S pacman-contrib --noconfirm --needed # for ranking mirrors
+  mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+  curl -sL "https://archlinux.org/mirrorlist/?country=LT&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+}
+
 pacman -S gptfdisk --noconfirm --needed
+function setupDisks {
+  echo -e "\nInstalling prereqs...\n$HR"
 
-echo "-------------------------------------------------"
-echo "-------select your disk to format----------------"
-echo "-------------------------------------------------"
-lsblk
-echo "Please enter disk: (example /dev/sda)"
-read DISK
-echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
-echo "--------------------------------------"
+  echo "-------------------------------------------------"
+  echo "-------select your disk to format----------------"
+  echo "-------------------------------------------------"
+  lsblk
+  echo "Please enter disk: (example /dev/sda)"
+  read DISK
+  echo "--------------------------------------"
+  echo -e "\nFormatting disk...\n$HR"
+  echo "--------------------------------------"
 
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+  # disk prep
+  sgdisk -Z ${DISK} # zap all on disk
+  sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
-# create partitions
-sgdisk -n 1:0:+1000M ${DISK} # boot partition
-sgdisk -n 2:0:+2000M ${DISK} # swap partition
-sgdisk -n 3:0:-10G   ${DISK} # main partition
+  # create partitions
+  sgdisk -n 1:0:+1000M ${DISK} # boot partition
+  sgdisk -n 2:0:+2000M ${DISK} # swap partition
+  sgdisk -n 3:0:-10G   ${DISK} # main partition
 
-sgdisk -t 1:ef00 ${DISK} # Efi system partition
-sgdisk -t 2:8200 ${DISK} # Linux swap
-sgdisk -t 1:8304 ${DISK} # Linux x86-x64 root (/)
+  sgdisk -t 1:ef00 ${DISK} # Efi system partition
+  sgdisk -t 2:8200 ${DISK} # Linux swap
+  sgdisk -t 1:8304 ${DISK} # Linux x86-x64 root (/)
 
-# label partitions
-sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"SWAP"    ${DISK}
-sgdisk -c 3:"ROOT"    ${DISK}
+  # label partitions
+  sgdisk -c 1:"UEFISYS" ${DISK}
+  sgdisk -c 2:"SWAP"    ${DISK}
+  sgdisk -c 3:"ROOT"    ${DISK}
 
-# sgdisk -p /dev/sda
+  # sgdisk -p /dev/sda
 
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
+  # make filesystems
+  echo -e "\nCreating Filesystems...\n$HR"
 
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+  mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
 
-mkswap "${DISK}2"
-swapon "${DISK}2"
+  mkswap "${DISK}2"
+  swapon "${DISK}2"
 
-cryptsetup -y -v luksFormat "${DISK}3"
-cryptsetup open "${DISK}3" cryptroot
-mkfs.ext4 /dev/mapper/cryptroot
-mount /dev/mapper/cryptroot /mnt
+  cryptsetup -y -v luksFormat "${DISK}3"
+  cryptsetup open "${DISK}3" cryptroot
+  mkfs.ext4 /dev/mapper/cryptroot
+}
 
-mkdir /mnt/boot
-mount "${DISK}1" /mnt/boot
+function prepareDisks {
+  mount /dev/mapper/cryptroot /mnt
 
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo --noconfirm --needed
+  mkdir /mnt/boot
+  mount "${DISK}1" /mnt/boot
+  pacstrap /mnt base base-devel linux linux-firmware vim nano sudo --noconfirm --needed
 
-genfstab -U /mnt >> /mnt/etc/fstab
+  genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+#----------------------------------------------
+setupMirrors
+setupDisks
+prepareDisks
 
 cp Installers/archChroot.sh /mnt/root
-
 arch-chroot /mnt /root/archChroot.sh
 
 umount -R /mnt
--- Installers/archChroot.sh
+++ Installers/archChroot.sh
@@ -1,52 +1,62 @@
 #!/usr/bin/env bash
 
-
 pacman -Syy
 
+function setupMirrors {
+  pacman -S reflector --noconfirm --needed
+  cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+  reflector --country Lithuania -p http --sort delay --save /etc/pacman.d/mirrorlist
+}
+
+function prepareBootConfiguration {
+  # set cryptodevice info
+  sed -i.bak 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="cryptdevice=\/dev\/sda3:cryptroot"/' /etc/default/grub
+
+  # add encrypt module
+  sed -i.bak 's/HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+
+  mkinitcpio -p linux-lts
+
+  grub-install /dev/sda --recheck --efi-directory=/boot
+  grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+function setupNetwork {
+  # for internet to be available on next run
+  pacman -S dhcpcd --noconfirm --noconfirm --needed
+  systemctl enable dhcpcd
+
+  # SETTINGS
+  echo "nameserver 1.1.1.1" >> /etc/resolvconf.conf
+
+  pacman -S openssh --noconfirm --needed
+  cat /etc/ssh/sshd_config | grep  "^PermitRootLogin"
+  if [[ $? == 1 ]]; then
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+  else
+    sed -i.bak 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/ssh_config
+  fi
+
+  systemctl enable --now sshd
+}
+
+function setupUsers {
+  [[ $(passwd --status root | awk '{print $2}') == 'P' ]] || passwd
+
+  # create secondary user
+  id dago >/dev/null 2>&1
+  [[ $? == 0 ]] || useradd -m dago
+
+  [[ $(passwd --status dago | awk '{print $2}') == 'P' ]] || (echo -e "Dago password:\n"; passwd dago)
+}
+#----------------------------------------------
+setupMirrors
 pacman -S neovim --noconfirm --needed
-
-pacman -S reflector --noconfirm --needed
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-reflector --country Lithuania -p http --sort delay --save /etc/pacman.d/mirrorlist
-
 pacman -S linux-headers linux-lts linux-lts-headers --noconfirm --needed
 pacman -S grub efibootmgr --noconfirm --needed
 
-# set cryptodevice info
-sed -i.bak 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="cryptdevice=\/dev\/sda3:cryptroot"/' /etc/default/grub
-
-# add encrypt module
-sed -i.bak 's/HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/' /etc/mkinitcpio.conf
-
-mkinitcpio -p linux-lts
-
-grub-install /dev/sda --recheck --efi-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# for internet to be available on next run
-pacman -S dhcpcd --noconfirm --noconfirm --needed
-systemctl enable dhcpcd
-
-# SETTINGS
-echo "nameserver 1.1.1.1" >> /etc/resolvconf.conf
-
-# change root password
-[[ $(passwd --status root | awk '{print $2}') == 'P' ]] || passwd
-
-pacman -S openssh --noconfirm --needed
-cat /etc/ssh/sshd_config | grep  "^PermitRootLogin"
-if [[ $? == 1 ]]; then
-  echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-else
-  sed -i.bak 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/ssh_config
-fi
-
-systemctl enable --now sshd
-
-# create secondary user
-id dago >/dev/null 2>&1
-[[ $? == 0 ]] || useradd -m dago
-
-[[ $(passwd --status dago | awk '{print $2}') == 'P' ]] || (echo -e "Dago password:\n"; passwd dago)
+prepareBootConfiguration
+setupUsers
+setupNetwork
 
 exit
